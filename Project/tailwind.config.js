ff4463674138e63012207ee5ecef3be5c3d72e54
Pranavp1101 <107991540+Pranavp1101@users.noreply.github.com>
2024-01-19 10:20:54 +0530

    Added feed, login and register page
    All html files in public folder, tailwind base stylesheet in src folder

---
+++ Project/tailwind.config.js
@@ -0,0 +1,17 @@
+/** @type {import('tailwindcss').Config} */
+module.exports = {
+  content: [
+    "C:/Users/Pranav/Desktop/College/Project/src/**/*.{html,js}",
+    "C:/Users/Pranav/Desktop/College/Project/public/**/*.{html,js}"
+],
+  theme: {
+    extend: {
+      colors:{
+        "primary": "#0B1416",
+        "secondary": "#1A282D"
+      }
+    },
+  },
+  plugins: [],
+}
+
