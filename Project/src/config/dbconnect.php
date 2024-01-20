80440683f7389415c487aeef3f01704f48fa822b
nikumyash <98515855+nikumyash@users.noreply.github.com>
2024-01-20 09:06:49 +0530

    changed db to localhost

--- Project/src/config/dbconnect.php
+++ Project/src/config/dbconnect.php
@@ -1,5 +1,5 @@
 <?php
-    $connection = mysqli_connect("34.93.160.208","root","scsphp","SCS_php");
+    $connection = mysqli_connect("localhost","root","","SCS_php");
     if(mysqli_connect_errno()){
         echo "Error:-". mysqli_connect_error();
     }
